730621ef1c244b2966e4cc5a21d93f834127b90e
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2023-06-21 20:20:11 +0200

    feat: added functions to run and test PH models

--- R/coh_effectiveness.R
+++ R/coh_effectiveness.R
@@ -1,11 +1,13 @@
 library(survival)
-#' Function to test the proportional hazards hypothesis of the model.
-#' The function relies on the implementation of the Scheunfeld test for testing
-#' the proportional hazards hypothesis on survival package. 
-#' It returns the result of the test and it can by plotted using plot().
-#' If the test is rejected, it is recomended to try other statistical 
-#' strategies, e.g. stratifying the dataset by confounders or including 
-#' time-dependent variables.
+#' Function to estimate the vaccine effectiveness based on the vaccination 
+#' status.
+#' The function relies on the implementation of the Cox model for proportional
+#' hazards on survival package. It returns a dataframe with the summary of the
+#' estimation that includes the value of the hazard ratio, the vaccine
+#' effectiveness, and the result of the test for the Proportional Hazards 
+#' hypothesis. If the test is rejected, it is recomended to try other 
+#' statistical strategies, e.g. stratifying the dataset by confounders or 
+#' including time-dependent variables.
 #' @param data dataset with at least one column to generate the status
 #' @param outcome_status_col name of the column containin status of the 
 #' event (most be a binary column)
@@ -35,20 +37,39 @@ library(survival)
 #' "2021-01-01", "2021-12-31",
 #' FALSE) 
 #' 
-#' test <- coh_test_noconf(cohortdata,
+#' coh_eff_noconf(cohortdata,
 #' "death.status",
 #' "time.to.death",
 #' "vaccine.status")
-#' plot(test)
-#' 
 #' }
 #' @export
-coh_test_noconf <- function(data, outcome_status_col,
+coh_eff_noconf <- function(data, outcome_status_col,
                         time_to_event_col,
                         status_vacc_col,
                         p_thr = 0.05) {
     cx <- coxph(Surv(data[[time_to_event_col]], data[[outcome_status_col]])
                     ~ data[[status_vacc_col]])
-    test <- cox.zph(cx)
-    return(test)
+    test <- cox.zph(cx)    
+    hr <- c(round(exp(coef(cx)), 4)[1])
+    ci025 <- c(round(exp(confint(cx)), 4)[1])
+    ci975 <- c(round(exp(confint(cx)), 4)[2])
+    p <- test$table[5]
+    p_value <- c(format(p, digits = 3))
+    if (p < p_thr) {
+        ph <- c("reject")
+    } else {
+        ph <- c("accept")
+    }
+
+    df_summ <- data.frame(
+                HR = hr,
+                HR.low = ci025,
+                HR.high = ci975,
+                V.eff = 1 - hr,
+                V.eff.low = 1 - ci975,
+                V.eff.high = 1 - ci025,
+                PH = ph,
+                p.value = p_value
+                )
+    return(df_summ)
 }
--- dolab/test_cohorts.R
+++ dolab/test_cohorts.R
@@ -1,33 +1,37 @@
 source("R/vaccineff_package.R")
 source("R/all_data_prep.R")
 source("R/coh_data_wrangling.R")
+source("R/coh_effectiveness.R")
+source("R/coh_performance.R")
+
 cohortdata <- readRDS("data/cohortdata.rds")
 head(cohortdata)
 str(cohortdata)
 
 cohortdata$age.group <- get_age_group(cohortdata, "age", 80, 9)
 table(cohortdata$age.group)
-
-cohortdata$vaccine.status <- set_status(cohortdata, 
-                                        c("vaccine.date.1", "vaccine.date.2"), 
-                                        status = c("v", "u"))
-table(cohortdata$vaccine.status)
-
-source("R/coh_data_wrangling.R")
 cohortdata$immunization.death <- get_immunization_date(cohortdata, "death.date", 0, 14,
                                                         c("vaccine.date.1", "vaccine.date.2"), 
                                                         "2021-12-31", take_first = FALSE)
-head(cohortdata)
-head(cohortdata %>% filter(!(is.na(death.date))))
-head(cohortdata %>% filter(is.na(death.date)))
-
-source("R/coh_data_wrangling.R")
+cohortdata$vaccine.status <- set_status(cohortdata, 
+                                        c("immunization.death"), 
+                                        status = c("v", "u"))
+cohortdata$death.status <- set_status(cohortdata, 
+                                    c("death.date"))
 cohortdata$time.to.death <- get_time_to_event(cohortdata, "death.date",
                                             "2021-01-01", "2021-12-31", 
-                                            TRUE, "immunization.death") 
-head(cohortdata)
-head(cohortdata %>% filter(!(is.na(death.date))))
-
+                                            FALSE) 
+coh_eff_noconf(cohortdata, 
+                "death.status",
+                "time.to.death",
+                "vaccine.status")
+source("R/coh_performance.R")
+t<- coh_test_noconf(cohortdata, 
+                "death.status",
+                "time.to.death",
+                "vaccine.status")
+plot(t)
+t
 source("R/coh_data_wrangling.R")
 cohortdata$immunization.dose <- get_immunization_dose(cohortdata, "immunization.death", 
                                                         c("vaccine.date.1", "vaccine.date.2"),

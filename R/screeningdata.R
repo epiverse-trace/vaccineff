8b4d4f09684a6c5b4993c54cec874e4e4694e54f
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2023-07-05 01:44:22 +0200

    feat: added example data set for screening method

---
+++ R/screeningdata.R
@@ -0,0 +1,15 @@
+#' Screening method data on vaccineff
+#'
+#' Data from
+#'
+#' @docType data
+#'
+#' @usage screeningdata
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' screeningdata
+"screeningdata"

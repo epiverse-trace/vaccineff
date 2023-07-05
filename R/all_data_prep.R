12200e8f9db9223a4e562193836e72bc3b7b9ad6
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-07-05 15:54:15 -0500

    Update documentation get_age_groups()

--- R/all_data_prep.R
+++ R/all_data_prep.R
@@ -1,32 +1,40 @@
-#' Method for constructing age-group variable from age column
-#' This method splits an age interval from min_val to max_val into
-#' (max_val-min_val)/step intervals.
-#' By default min_val is set 0, however it can be assigned by
+#' @title Construct age-group variable from age column
+#'
+#' @description This method splits an age interval from `min_val` to `max_val`
+#' into `(max_val - min_val) / step` intervals.
+#' By default `min_val` is set to 0, however it can be assigned by
 #' convenience.
-#' If the method finds ages greater or equal than max_val
-#' it assigns the string ">{max_val}".
-#' To avoid errors it is necessary to set step < max_val.
+#' If the method finds ages greater or equal than `max_val`
+#' it assigns the string `">max_val"`.
+#' To avoid errors it is necessary to set `step < max_val`.
 #' It is also suggested to choose the step such
-#' that max_val%%(step+1) = 0
+#' that `max_val %% (step + 1) == 0`.
+#'
 #' @param data dataset with at least a column containing the age
 #' information
 #' @param col_age name of the column containing the age
 #' information
-#' @param  max_val maximum value of age interval to split
-#' @param  step step used to split the age interval
-#' @param  min_val minimum value of age interval to split
-#' @return age_group
+#' @param max_val maximum value of age interval to split
+#' @param step step used to split the age interval
+#' @param min_val minimum value of age interval to split
+#' @return A `factor` object of the same length as the number of rows in `data`,
+#' with levels corresponding to age bins between `min_val` and `max_val`.
+#' Ages above `max_val` are represented as `>max_val`.
+#' @export
 #' @examples
-#' \dontrun{
-#' cohortdata <- data(cohortdata)
-#' cohortdata$age.group <- get_age_group(
+#' # load data provided with the package
+#' data(cohortdata)
+#'
+#' # assign age groups as a column of the data frame
+#' cohortdata$age_group <- get_age_group(
 #'   data = cohortdata,
 #'   col_age = "age",
 #'   max_val = 80,
 #'   step = 9
 #' )
-#' }
-#' @export
+#'
+#' # view the data frame with new column
+#' head(cohortdata)
 get_age_group <- function(data, col_age, max_val, min_val = 0, step) {
   # input checking
   checkmate::assert_data_frame(

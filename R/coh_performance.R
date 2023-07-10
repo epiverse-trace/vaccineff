3982603df7f2adcfc711eb0144f2fff37eb59c79
Pratik Gupte <pratik.gupte@lshtm.ac.uk>
2023-07-10 13:56:56 -0500

    Input checking, inherit params for fn coh_test_noconf()

--- R/coh_performance.R
+++ R/coh_performance.R
@@ -6,22 +6,18 @@
 #' If the test is rejected, it is recomended to try other statistical
 #' strategies, e.g. stratifying the dataset by confounders or including
 #' time-dependent variables.
-#' @param data dataset with cohort information (see example)
-#' @param outcome_status_col name of the column containing status of the
-#' event (most be a binary column)
-#' @param time_to_event_col name of the column containing the time-to-event
-#' @param status_vacc_col name of the column containing the vaccination
-#' status
-#' @param p_thr p-value to test Proportional Hazards Hypothesis
+#' @inheritParams coh_eff_noconf
 #' @return Schoenfeld test: Survival element
 #' @examples
-#' \dontrun{
+#' # load example package data
 #' data("cohortdata")
+#'
+#' # add useful columns
 #' cohortdata$immunization_death <- get_immunization_date(
-#'   date = cohortdata,
+#'   data = cohortdata,
 #'   outcome_date_col = "death_date",
 #'   outcome_delay = 0,
-#'   immunisation_delay = 14,
+#'   immunization_delay = 14,
 #'   vacc_date_col = c("vaccine_date_1", "vaccine_date_2"),
 #'   end_cohort = "2021-12-31",
 #'   take_first = FALSE
@@ -46,24 +42,39 @@
 #'   FALSE
 #' )
 #'
+#' # perform the test
 #' test <- coh_test_noconf(
 #'   cohortdata,
 #'   "death_status",
 #'   "time_to_death",
 #'   "vaccine_status"
 #' )
-#' plot(test)
-#' }
+#'
+#' # View test object
+#' test
 #' @export
 coh_test_noconf <- function(data, outcome_status_col,
                             time_to_event_col,
                             status_vacc_col,
                             p_thr = 0.05) {
-  cx <- survival::coxph(survival::Surv(
-    data[[time_to_event_col]],
-    data[[outcome_status_col]]
+  # input checking
+  checkmate::assert_data_frame(
+    data,
+    min.rows = 1L
   )
-  ~ data[[status_vacc_col]])
+  checkmate::assert_names(
+    names(data),
+    must.include = c(outcome_status_col, time_to_event_col, status_vacc_col)
+  )
+  checkmate::assert_number(p_thr, lower = 0.0, upper = 1.0)
+
+  # create survival object, exclude from object usage linting
+  indiv_survival <- survival::Surv( # nolint
+    data[[time_to_event_col]], data[[outcome_status_col]]
+  )
+
+  cx <- survival::coxph(indiv_survival ~ data[[status_vacc_col]])
   test <- survival::cox.zph(cx)
+
   return(test)
 }

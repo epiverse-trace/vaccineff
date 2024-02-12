d9fe2a63ebd0ddd1cd83fc1d187739fdbba4ca9e
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-02-12 18:30:30 +0100

    feat: test for censoring_date_col

--- tests/testthat/test-time_to_event.R
+++ tests/testthat/test-time_to_event.R
@@ -58,3 +58,53 @@ test_that("`get_time_to_event`: Basic expectations", {
     all(time_to_death >= 0 | is.na(time_to_death))
   )
 })
+
+# Tests for censoring
+test_that("`get_time_to_event`: Censoring provided", {
+  # cohort start and end time
+  start_cohort <- as.Date("2044-01-01")
+  end_cohort <- as.Date("2044-12-31")
+
+  # assign immunization date
+  data$immunization_c <- get_immunization_date(
+    data = cohortdata,
+    outcome_date_col = "death_date",
+    censoring_date_col = "death_other_causes",
+    outcome_delay = 0,
+    immunization_delay = 14,
+    vacc_date_col = c("vaccine_date_1", "vaccine_date_2"),
+    end_cohort = as.Date("2044-12-31"),
+    take_first = FALSE
+  )
+
+  # calculate time to death using censoring data
+  time_to_death_c <- get_time_to_event(
+    data = data,
+    outcome_date_col = "death_date",
+    censoring_date_col = "death_other_causes",
+    start_cohort = start_cohort,
+    end_cohort = end_cohort,
+    start_from_immunization = TRUE,
+    immunization_date_col = "immunization_c"
+  )
+
+  # expect numeric vector
+  expect_vector(
+    time_to_death_c,
+    ptype = numeric()
+  )
+  expect_true(
+    all(time_to_death_c >= 0 | is.na(time_to_death_c))
+  )
+
+  data$time_to_death_c <- time_to_death_c
+  informed <- data[!is.na(data$death_other_causes) &
+                     !is.na(data$immunization_c), ]
+
+  expect_true(
+    all(informed$time_to_death_c ==
+          informed$death_other_causes - informed$immunization_c
+    )
+  )
+
+})

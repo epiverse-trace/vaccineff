67d49aa5b35de1415d30560145d503fd507936dd
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-02-13 13:03:46 +0100

    feat: warning for module of interval by step different to zero. Updated documentation of the function

--- R/all_data_prep.R
+++ R/all_data_prep.R
@@ -1,14 +1,15 @@
 #' @title Construct age-group variable from age column
 #'
 #' @description This method splits an age interval from `min_val` to `max_val`
-#' into `(max_val - min_val) / step` intervals.
-#' By default `min_val` is set to 0, however it can be assigned by
-#' convenience.
+#' into intervals of size `step``.ç
 #' If the method finds ages greater or equal than `max_val`
 #' it assigns the string `">max_val"`.
-#' To avoid errors it is necessary to set `step < max_val`.
-#' It is also suggested to choose the step such
-#' that `max_val %% (step + 1) == 0`.
+#' By default `min_val` is set to 0, however it can be assigned by
+#' convenience. If the method finds ages lower or equal
+#' than `min_val` it assigns the string `"<min_val-1"`.
+#' The function warns when (max_val - min_val) %% step != 0. In that case
+#' the last interval is truncated to the closest upper value to max_val that
+#' satisfies (closest_upper - min_val) %% step == 0.
 #'
 #' @param data dataset with at least a column containing the age
 #' information
@@ -64,7 +65,14 @@ get_age_group <- function(data, col_age, max_val, min_val = 0, step) {
   }  else {
     lim_labels <- c(paste0("<", min_val - 1), lim_labels)
     lim_breaks <- c(-Inf, limits_low[seq(1, length(limits_low))] - 1, Inf)
+  }
 
+  #Warning of module condition not satisfied
+  if ((max_val - min_val) %% step != 0) {
+    war_msg <- "The values provided do not satisfy (max_val-min_val)%%step = 0.
+    The last interval will be truncated to "
+    war_msg <- paste0(war_msg, lim_labels[length(lim_labels)])
+    warning(war_msg)
   }
 
   # cut the age data and apply labels

394eb83e9678f4c87699c6ec25a099db52732fef
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-02-24 12:24:00 +0100

    style: documentation

--- R/all_data_prep.R
+++ R/all_data_prep.R
@@ -1,15 +1,15 @@
 #' @title Construct age-group variable from age column
 #'
 #' @description This method splits an age interval from `min_val` to `max_val`
-#' into intervals of size `step``.ç
+#' into intervals of size `step`.
 #' If the method finds ages greater or equal than `max_val`
 #' it assigns the string `">max_val"`.
 #' By default `min_val` is set to 0, however it can be assigned by
 #' convenience. If the method finds ages lower or equal
 #' than `min_val` it assigns the string `"<min_val-1"`.
-#' The function warns when (max_val - min_val) %% step != 0. In that case
-#' the last interval is truncated to the closest upper value to max_val that
-#' satisfies (closest_upper - min_val) %% step == 0.
+#' The function warns when (max_val - min_val) is not an integer multiple of
+#' step. In that case the last interval is truncated to the upper value
+#' closest to max_val for which (closest_upper - min_val) is multiple of step.
 #'
 #' @param data dataset with at least a column containing the age
 #' information

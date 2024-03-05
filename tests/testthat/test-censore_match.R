298eae8c9a71dc758ee3b9043a7e699658eb7628
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-03-05 12:38:27 +0100

    feat: test for basic expectations and minimum censoring date

---
+++ tests/testthat/test-censore_match.R
@@ -0,0 +1,56 @@
+#### Tests for censore_match()
+
+## prepare data
+data("cohortdata")
+start_cohort <- as.Date("2044-01-01")
+end_cohort <- as.Date("2044-12-31")
+
+# sample cohort to make tests faster - take a bigger sample
+sample_size <- 50000
+sample_indices <- sample(nrow(cohortdata), sample_size)
+sample_cohort <- cohortdata[sample_indices, ]
+rownames(sample_cohort) <- NULL
+
+# assign vaccination status
+sample_cohort$vaccine_status <- set_status(
+  data = sample_cohort,
+  col_names = "vaccine_date_2",
+  status = c("v", "u")
+)
+# match cohort
+matched_cohort <- match_cohort(data = sample_cohort,
+  status_vacc_col = "vaccine_status",
+  exact = "sex"
+)
+# add column with censoring date for match
+matched_cohort$censoring_date <-  censore_match(
+  data = matched_cohort,
+  censoring_date_col = "death_other_causes"
+)
+
+# snapshot test basic expectations
+test_that("`censore_match`: basic expectations", {
+  # filter registers with censoring date informed
+  censored_original <-
+    matched_cohort[!is.na(matched_cohort$death_other_causes), ]
+  # filter registers with censoring date created after matching
+  censored_match <-
+    matched_cohort[!is.na(matched_cohort$censoring_date), ]
+
+  # All the subclass IDs in censored_match must be contained in
+  # censored_original
+  expect_true(
+    all(censored_match$subclass %in% censored_original$subclass)
+  )
+})
+
+test_that("`censore_match`: take minimum censoring date", {
+  censored_twodates <-
+    matched_cohort[!is.na(matched_cohort$censoring_date)  &
+      !is.na(matched_cohort$death_other_causes),
+    ]
+
+  expect_true(
+    all(censored_twodates$censoring_date <= censored_original$censored_twodates)
+  )
+})

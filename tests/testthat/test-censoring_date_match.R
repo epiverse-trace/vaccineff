19d8bf6963d419a0a223de8771195a332cdd585c
David Santiago Quevedo <67650252+davidsantiagoquevedo@users.noreply.github.com>
2024-03-13 16:37:30 +0100

    Feat matching (#45)
    * feat: match function
    
    * refac: added MatchtIt to Description
    
    * feat: documentation, removed  in output. Changed name of function
    
    * roxygen: generate documentation match_cohort
    
    * feat: added basic estructure of function to censore matches
    
    * Fix 38 age interval get age group (#43)
    
    * fix: definition of age interval when (max_val-min_val)%%setp !=0. Added option to set interval '<min_val' when min_val<0
    
    * refac: removed test forcing interval '>max_val', when (max_val-min_val)%%step!=0. Added test for min_val>0
    
    * refac: step=10 on PH-accept test
    
    * feat: pulled description from previous PR
    
    * feat: warning for module of interval by step different to zero. Updated documentation of the function
    
    * simplified tests to control warning
    
    * roxygen: description of warning and lower value
    
    * fix: assert_number replaced by assert_integerish
    
    * fix: upper = max_val for assertion on min_val
    
    * style: documentation
    
    * fix: warning message for (max-min)%%step!=0
    
    * roxygen: automatic update
    
    * fix: assert_integerish replaced by assert_int, better for integer numbers. Removed upper check for min_val to avoid redundancy with lower check for max_mal
    
    * fix: example in documentation
    
    * feat: test for non-integer values
    
    * refac: simplified workflow of match_cohort. Set method = nearest by default and ratio = 1 in matchit
    
    * fix: documentation reflecting changes in match_cohort()
    
    * roxygen: refac match_cohort
    
    * feat: documentation censore_match
    
    * roxygen: documentation censore_cohort
    
    * fix: example in documentation of both functions and column selection excluding weights to pass gp()
    
    * roxygen: fixed examples
    
    * feat: test for basic expectations and minimum censoring date
    
    * fix: return ordered datetype column matching with order of data frame
    
    * fix: test for minimum values. Added test for datetype
    
    * feat: tests for match_cohort
    
    * added MatchIt to wordlist
    
    * refac: replaced 'censore' by 'censor'. This misspelling had propagated throughout all the new functions
    
    * removed duplicated test
    
    * refac: added control for censoring date of matches. No censoring if member of couple presents outcome before censoring date
    
    * feat: test for outcome date before censoring date
    
    * sytle: lintr line 73
    
    * roxygen: changes in documentation by adding outcome_date_col to params
    
    * feat: input checks censor_match
    
    * refac: censor_match() renamed as get_censoring_date_match
    
    * refac: removed caliper from input.  receives it now

---
+++ tests/testthat/test-censoring_date_match.R
@@ -0,0 +1,81 @@
+#### Tests for get_censoring_date_match()
+
+## prepare data
+data("cohortdata")
+start_cohort <- as.Date("2044-01-01")
+end_cohort <- as.Date("2044-12-31")
+
+# sample cohort to make tests faster - take a bigger sample
+sample_size <- 5000
+sample_indices <- sample(nrow(cohortdata), sample_size)
+sample_cohort <- cohortdata[sample_indices, ]
+rownames(sample_cohort) <- NULL
+
+# assign vaccination status
+sample_cohort$vaccine_status <- set_status(
+  data = sample_cohort,
+  col_names = "vaccine_date_2",
+  status = c("v", "u")
+)
+# match cohort
+matched_cohort <- match_cohort(data = sample_cohort,
+  status_vacc_col = "vaccine_status",
+  exact = "sex"
+)
+# add column with censoring date for match
+matched_cohort$censoring_date <- get_censoring_date_match(
+  data = matched_cohort,
+  outcome_date_col = "death_date",
+  censoring_date_col = "death_other_causes"
+)
+
+# snapshot test basic expectations
+test_that("`get_censoring_date_match`: basic expectations", {
+  # expect date
+  expect_vector(
+    matched_cohort$censoring_date,
+    ptype = as.Date("2045-01-01")
+  )
+  # filter registers with censoring date informed
+  censored_original <-
+    matched_cohort[!is.na(matched_cohort$death_other_causes), ]
+  # filter registers with censoring date created after matching
+  censored_match <-
+    matched_cohort[!is.na(matched_cohort$censoring_date), ]
+
+  # All the subclass IDs in censored_match must be contained in
+  # censored_original
+  expect_true(
+    all(censored_match$subclass %in% censored_original$subclass)
+  )
+})
+
+# Always take the minimum censoring date per couple
+test_that("`get_censoring_date_match`: take minimum censoring date", {
+  censored_twodates <-
+    matched_cohort[!is.na(matched_cohort$censoring_date)  &
+      !is.na(matched_cohort$death_other_causes),
+    ]
+
+  expect_true(
+    all(
+      censored_twodates$censoring_date <= censored_twodates$death_other_causes
+    )
+  )
+})
+
+# If one of the members of the couple has an outcome date before
+# the censoring date of the other. Do not censor, set censoring date
+# for the member with outcome as NA
+test_that("`get_censoring_date_match`: not censoring cases", {
+  censored_and_outcome <-
+    matched_cohort[!is.na(matched_cohort$censoring_date)  &
+      !is.na(matched_cohort$death_date),
+    ]
+
+  expect_true(
+    all(
+      censored_and_outcome$censoring_date > censored_and_outcome$death_date
+    )
+  )
+})
---
+++ tests/testthat/test-match_cohort.R
@@ -0,0 +1,127 @@
+#### Tests for get_censoring_date_match()
+
+## prepare data
+data("cohortdata")
+start_cohort <- as.Date("2044-01-01")
+end_cohort <- as.Date("2044-12-31")
+
+# sample cohort to make tests faster - take a bigger sample
+sample_size <- 1000
+sample_indices <- sample(nrow(cohortdata), sample_size)
+sample_cohort <- cohortdata[sample_indices, ]
+rownames(sample_cohort) <- NULL
+
+# assign vaccination status
+sample_cohort$vaccine_status <- set_status(
+  data = sample_cohort,
+  col_names = "vaccine_date_2",
+  status = c("v", "u")
+)
+
+# test for basic expectations - all provided
+test_that("`match_cohort`: basic expectations - all provided", {
+  matched_cohort <- match_cohort(
+    data = sample_cohort,
+    status_vacc_col = "vaccine_status",
+    exact = "sex",
+    nearest = c(age = 1)
+  )
+  # expect dataframe
+  expect_s3_class(matched_cohort, "data.frame")
+
+  # error argumentes not provided
+  expect_error(
+    match_cohort(
+      data = sample_cohort,
+      status_vacc_col = "vaccine_status"
+    ),
+    regexp =
+      "`exact` and `nearest` cannot be NULL. At least one must be provided"
+  )
+
+  # keep all the columns and add only "subclass" and "prop_score"
+  expect_true(
+    all(
+      names(matched_cohort) %in%
+        c(names(sample_cohort), "subclass", "prop_score")
+    )
+  )
+
+  # even number of couples
+  expect_setequal(
+    nrow(matched_cohort),
+    length(unique(matched_cohort$subclass)) * 2
+  )
+
+  # same number of vaccinated and unvaccinated
+  expect_identical(
+    nrow(matched_cohort[matched_cohort$vaccine_status == "u", ]),
+    nrow(matched_cohort[matched_cohort$vaccine_status == "v", ])
+  )
+
+  # same number of sex in "v" and "u"
+  expect_identical(
+    table(matched_cohort[matched_cohort$vaccine_status == "u", ]$sex),
+    table(matched_cohort[matched_cohort$vaccine_status == "v", ]$sex)
+  )
+
+  # same number of sex in "v" and "u"
+  expect_identical(
+    table(matched_cohort[matched_cohort$vaccine_status == "u", ]$sex),
+    table(matched_cohort[matched_cohort$vaccine_status == "v", ]$sex)
+  )
+
+  # ages are not equal
+  expect_false(
+    isTRUE(
+      all.equal(
+        table(matched_cohort[matched_cohort$vaccine_status == "u", ]$age),
+        table(matched_cohort[matched_cohort$vaccine_status == "v", ]$age)
+      )
+    )
+  )
+})
+
+# test for exact match
+test_that("`match_cohort`: exact match", {
+  matched_cohort <- match_cohort(
+    data = sample_cohort,
+    status_vacc_col = "vaccine_status",
+    exact = "sex"
+  )
+  # same number of categories in "v" and "u"
+  expect_identical(
+    table(matched_cohort[matched_cohort$vaccine_status == "u", ]$sex),
+    table(matched_cohort[matched_cohort$vaccine_status == "v", ]$sex)
+  )
+})
+
+# test for exact match
+test_that("`match_cohort`: nearest match", {
+  matched_cohort <- match_cohort(
+    data = sample_cohort,
+    status_vacc_col = "vaccine_status",
+    nearest = c(age = 3)
+  )
+  # even number of couples
+  expect_setequal(
+    nrow(matched_cohort),
+    length(unique(matched_cohort$subclass)) * 2
+  )
+
+  # same number of vaccinated and unvaccinated
+  expect_identical(
+    nrow(matched_cohort[matched_cohort$vaccine_status == "u", ]),
+    nrow(matched_cohort[matched_cohort$vaccine_status == "v", ])
+  )
+
+  # ages are not equal due to caliper
+  expect_false(
+    isTRUE(
+      all.equal(
+        table(matched_cohort[matched_cohort$vaccine_status == "u", ]$age),
+        table(matched_cohort[matched_cohort$vaccine_status == "v", ]$age)
+      )
+    )
+  )
+})

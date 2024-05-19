61e9f74b627902dc50779ea53640145d786795ee
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-19 17:59:42 +0200

    feat: documentation match_cohort

--- R/coh_match.R
+++ R/coh_match.R
@@ -1,3 +1,46 @@
+#' @title Match Cohort to Reduce Observational Bias
+#'
+#' @description This function builds couples of vaccinated and unvaccinated
+#' individuals with similar characteristics. The function relies on the
+#' matching algorithm implemented in the `{MatchIt}` package, setting,
+#' by default, `method = "nearest"`, `ratio = 1`,
+#' and `distance = "mahalanobis"`.
+#' Exact and near characteristics are accepted, passed in the parameters
+#' `exact` and `nearest`, respectively. The parameter `nearest` must be provided
+#' together with the calipers as a named vector
+#' (e.g., `nearest = c(characteristic1 = n1, characteristic2 = n2)`,
+#' where `n1` and `n2` are the calipers).
+#' The default matching `method` of the function is `static`. This means that
+#' couples are matched once, without taking into account their vaccination,
+#' censoring, and outcome dates. After this, the couples whose exposition times
+#' do not coincide are removed to avoid negative time-to-events.
+#' The function returns a matched and adjusted by exposition cohort, with
+#' information of the beginning of follow-up period (`t0_follow_up`) of couples,
+#' corresponding to the vaccination date of the vaccinated individual,
+#' the end of the follow-up period (`tf_follow_up`) of each individual,
+#' and their time-to-event (`time_to_event`).
+#' Couples are censored if the vaccinated or unvaccinated partner was previously
+#' censored (i.e., if `censoring_date_col` is informed) and the censor occurs
+#' before their outcomes.
+#' Rolling calendar matching method will be included in future releases.
+#'
+#' @inheritParams coh_effectiveness
+#' @param immunization_date Name of the column that contains the immunization
+#' date to set the beginning of the follow-up period (`t0_follow_up`).
+#' @param exact Name(s) of column(s) for `exact` matching. Default is `NULL`.
+#' @param nearest Named vector with name(s) of column(s) for `nearest` matching
+#' and caliper(s) for each variable (e.g., `nearest = c("characteristic1" = n1,
+#' "characteristic2" = n2)`, where `n1` and `n2` are the calipers). Default is
+#' `NULL`.
+#' @return Data frame with matched population. Three columns are added to the
+#' structure provided in `data`: `subclass` (ID of matched couple),
+#' `t0_follow_up` (beginning of follow-up period for couple),
+#' `tf_follow_up` (end of follow-up period, this includes censoring dates
+#' inherited from couple),
+#' and `time_to_event` (difference between `t0_follow_up` and `tf_follow_up`).
+#' @keywords internal
+
+
 match_cohort <- function(data,
                          outcome_date_col,
                          censoring_date_col,
@@ -22,7 +65,6 @@ match_cohort <- function(data,
   class(match_obj) <- "match"
   return(match_obj)
 }
-
 
 summary.match <- function(match) {
   # Check if the input object is of class "match"

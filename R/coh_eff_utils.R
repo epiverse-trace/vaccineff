e9e2ec3ba8f1155443de517e2ed2c500dc7098c8
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-28 18:15:46 +0200

    refac: split effectiveness into utils and cox model function for VE

---
+++ R/coh_eff_utils.R
@@ -0,0 +1,124 @@
+#' Internal function to extract summary output from survival models.
+#'
+#' @inheritParams effectiveness
+#' @param model survival object with model
+#' @return Data frame with survival data
+#' @keywords internal
+extract_surv_model <- function(model, start_cohort, end_cohort) {
+  days <- end_cohort - start_cohort
+  tte <- seq(0, as.numeric(days) - 1, by = 1)
+  res <- summary(model, times = tte, scale = 1)
+  cols <- lapply(c(2:6, 8:16), function(x) res[x])
+  tbl <- do.call(data.frame, cols)
+  tbl$date <- tbl$time + start_cohort
+  return(tbl)
+}
+
+#' Internal function to calculate Kapplan-Meier model and related metrics.
+#'
+#' @inheritParams effectiveness
+#' @return Data frame with data from KM model
+#' "time", "date", "strata",
+#' "n.risk", "n.event", "n.censor",
+#' "surv", "lower", "upper",
+#' "cumincidence", "cumincidence_lower", "cumincidence_upper"
+#' @keywords internal
+km_model <- function(data,
+                     outcome_status_col,
+                     time_to_event_col,
+                     vacc_status_col,
+                     vaccinated_status,
+                     unvaccinated_status,
+                     start_cohort,
+                     end_cohort) {
+  # KM model time to event, outcome ~ vaccine status
+  model <- survival::survfit(
+    survival::Surv(
+      data[[time_to_event_col]],
+      data[[outcome_status_col]]
+    ) ~ data[[vacc_status_col]]
+  )
+  # Extract data from survival element
+  km <- extract_surv_model(model, start_cohort, end_cohort)
+
+  # Construct cumulative incidence = 1 - S
+  km$cumincidence <- 1 - km$surv
+  km$cumincidence_lower <- 1 - km$upper
+  km$cumincidence_upper <- 1 - km$lower
+
+  # Construct strata data
+  km$strata <- factor(km$strata,
+    levels = c(
+      paste0("data[[vacc_status_col]]=", vaccinated_status),
+      paste0("data[[vacc_status_col]]=", unvaccinated_status)
+    )
+  )
+  levels(km$strata) <- c(vaccinated_status, unvaccinated_status)
+
+  km <- subset(km,
+    select = c("time", "date", "strata",
+      "n.risk", "n.event", "n.censor",
+      "surv", "lower", "upper",
+      "cumincidence", "cumincidence_lower", "cumincidence_upper"
+    )
+  )
+
+  return(km)
+}
+
+#' Internal function to calculate Cox-PH model and related metrics.
+#'
+#' @inheritParams effectiveness
+#' @return List with data from Cox model:
+#' hr - hazard ratio (CI95%)
+#' p_value
+#' survival object with model
+#' survival object with Schoenfeld test
+#' @keywords internal
+cox_model <- function(data,
+                      outcome_status_col,
+                      time_to_event_col,
+                      vacc_status_col,
+                      vaccinated_status,
+                      unvaccinated_status) {
+
+  # Prepare data for model
+  data[[vacc_status_col]] <- factor(
+    data[[vacc_status_col]],
+    levels = c(vaccinated_status, unvaccinated_status),
+    ordered = FALSE
+  )
+
+  data[[vacc_status_col]] <- stats::relevel(
+    data[[vacc_status_col]], ref = unvaccinated_status
+  )
+
+  # Cox model time to event, outcome ~ vaccine status
+  # Regression
+  model <- survival::coxph(
+    survival::Surv( # nolint
+      data[[time_to_event_col]], data[[outcome_status_col]]
+    ) ~ data[[vacc_status_col]]
+  )
+
+  ## Hazard ratio
+  hr <- round(exp(stats::coef(model)), digits = 4)
+  # CI(95%): extract first and second element as limits
+  lower <- round(exp(stats::confint(model)), 4)[1]
+  upper <- round(exp(stats::confint(model)), 4)[2]
+
+  ## Schoenfled test for Proportional Hazards hypothesis
+  test <- survival::cox.zph(model)
+  # extract from matrix by name
+  p_value <- round(test$table["GLOBAL", "p"], 4)
+
+  cx <- list(hr = hr,
+    lower = lower,
+    upper = upper,
+    p_value = p_value,
+    model = model,
+    test = test
+  )
+
+  return(cx)
+}

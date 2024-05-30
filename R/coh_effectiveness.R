cc9975384ac660a23d455676c0f188d0b3a56402
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-05-30 16:39:08 +0200

    refac: added default values for parameters and updated documentation

--- R/coh_effectiveness.R
+++ R/coh_effectiveness.R
@@ -1,82 +1,54 @@
-#' Function to estimate the vaccine effectiveness based on the vaccination
-#' status.
+#' @title Estimate Vaccine Effectiveness (VE)
 #'
-#' The function relies on the implementation of the Cox model for proportional
-#' hazards on survival package. It returns a dataframe with the summary of the
-#' estimation that includes the value of the hazard ratio (CI95%), the vaccine
-#' effectiveness (CI95%), and the result of the test for the Proportional
-#' Hazards hypothesis. If the test is rejected, it is recomended to try other
-#' statistical strategies, e.g. stratifying the dataset by confounders or
-#' including time-dependent variables.
+#' This function provides methods for estimating VE. It relies on the
+#' implementation of the Kaplan-Meier estimator and the Cox model for
+#' proportional hazards in the package `{survival}`. Currently, the default
+#' method of the function is `HR` (Hazard Ratio). Thus, the VE = 1 - HR, where
+#' HR is calculated using the Cox model. The proportional hazards hypothesis is
+#' tested using the Schoenfeld test, and the resultant p-value is provided in
+#' the results. Log-log plots are also generated using the Kaplan-Meier
+#' survival estimator to provide a visual test for the proportional hazards
+#' hypothesis. The functions uses the default name of columns
+#' `outcome_status`, `time_to_event` and `vaccine_status`, and the  default
+#' status names `v` and `u`. However, custom names can be provided through the
+#' parameters `outcome_status_col`, `time_to_event_col`, `vacc_status_col`,
+#' `vaccinated_status` and `unvaccinated_status`.
+#' The return is a list with the call and the name of the method used for
+#' the estimation of VE (CI95%), the result of the performance test, and
+#' a suitable plot for the method.
+#' The object returned is compatible with the methods `summary` and `plot`.
 #'
-#' @param data dataset with cohort information (see example)
-#' @param outcome_status_col name of the column containing status of the
-#' event (most be a binary column)
-#' @param time_to_event_col name of the column containing the time-to-event
-#' @param vacc_status_col name of the column containing the vaccination
-#' @param vaccinated_status string assigned to the vaccinated population
-#' @param unvaccinated_status string assigned to the unvaccinated population
-#' @param start_cohort start date of the study
-#' @param end_cohort end date of the study
-#' status
-#' @return vaccine effectiveness (CI95%), Schoenfeld test,
-#' Loglog plot for Proportional Hazards
-#' @examples
-#' # load example data from package
-#' data("cohortdata")
-#'
-#' # add immunization dates
-#' cohortdata$immunization <- get_immunization_date(
-#'   data = cohortdata,
-#'   outcome_date_col = "death_date",
-#'   outcome_delay = 0,
-#'   immunization_delay = 14,
-#'   vacc_date_col = c("vaccine_date_1", "vaccine_date_2"),
-#'   end_cohort = as.Date("2044-12-31"),
-#'   take_first = FALSE
-#' )
-#'
-#' # add vaccine status
-#' cohortdata$vaccine_status <- set_status(
-#'   data = cohortdata,
-#'   col_names = c("immunization"),
-#'   status = c("v", "u")
-#' )
-#'
-#' # add death status
-#' cohortdata$death_status <- set_status(
-#'   data = cohortdata,
-#'   col_names = c("death_date")
-#' )
-#'
-#' # add time to death
-#' cohortdata$time_to_death <- get_time_to_event(
-#'   data = cohortdata,
-#'   outcome_date_col = "death_date",
-#'   start_cohort = as.Date("2044-01-01"),
-#'   end_cohort = as.Date("2044-12-31"),
-#'   start_from_immunization = FALSE
-#' )
-#'
-#' # estimate vaccine effectiveness
-#' coh_effectiveness(
-#'   data = cohortdata,
-#'   outcome_status_col = "death_status",
-#'   time_to_event_col = "time_to_death",
-#'   vacc_status_col = "vaccine_status",
-#'   start_cohort = start_cohort,
-#'   end_cohort = end_cohort
-#' )
+#' @param data dataset with cohort information (see example).
+#' @param start_cohort start date of the study.
+#' @param end_cohort end date of the study.
+#' @param method method to estimate VE. Default is `HR`.
+#' @param outcome_status_col name of the column containing status of the event
+#' (must be a binary column). Default is `outcome_status`.
+#' @param time_to_event_col name of the column containing the time-to-event.
+#' Default is `time_to_event`.
+#' @param vacc_status_col name of the column containing the vaccination.
+#' Default is `vaccine_status`.
+#' @param vaccinated_status string assigned to the vaccinated population.
+#' Default is `v`.
+#' @param unvaccinated_status string assigned to the unvaccinated population.
+#' Default is `u`.
+#' @return `effectiveness` object: list with results from estimation of VE.
+#' `call`: call of survival method,
+#' `ve`: data frame with VE(CI95%),
+#' `test`: result from test of performane,
+#' `plot`: plot of method,
+#' `method`: name of the method used for the estimation.
 #' @export
+
 effectiveness <- function(data,
-                          outcome_status_col,
-                          time_to_event_col,
-                          vacc_status_col,
-                          vaccinated_status,
-                          unvaccinated_status,
                           start_cohort,
                           end_cohort,
-                          method = "HR") {
+                          method = "HR",
+                          outcome_status_col = "outcome_status",
+                          time_to_event_col = "time_to_event",
+                          vacc_status_col = "vaccine_status",
+                          vaccinated_status = "v",
+                          unvaccinated_status = "u") {
 
   # input checking
   checkmate::assert_data_frame(

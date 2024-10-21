9299bcb7b3651e8721c11ede5c095905ac599be5
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-10-21 12:23:40 +0200

    feat: added function to calculate exposition time. refac: adjust exposition using get_exposition_time and get_censoring_after_match

--- R/coh_match_adjust_expo.R
+++ R/coh_match_adjust_expo.R
@@ -6,7 +6,7 @@
 #'
 #' @inheritParams match_cohort
 #' @param matched_cohort `data.frame` with matched cohort from `match_cohort_`.
-#' @return `data.frame` with matched population and corrected exposure times.
+#' @return `data.frame` with matched population and corrected exposition times.
 #' @keywords internal
 
 adjust_exposition <- function(matched_cohort,
@@ -15,78 +15,83 @@ adjust_exposition <- function(matched_cohort,
                               immunization_date,
                               start_cohort,
                               end_cohort) {
-  # 1. Check that individual censoring occurs before event
-  matched_cohort$censoring_individual <- as.Date(ifelse(
-    (matched_cohort[[censoring_date_col]] <
-       matched_cohort[[outcome_date_col]]) |
-      is.na(matched_cohort[[outcome_date_col]]),
-    as.character(matched_cohort[[censoring_date_col]]),
-    as.Date(NA)
-  ))
 
-  # 2. Match minimum censoring date as censoring date for pair
-  matched_cohort$censoring_pair <-  as.Date(match_pair_info(
-    data_set = matched_cohort,
-    column_to_match = "censoring_individual",
-    criteria = "min"
-  ))
-
-  # 3. If an outcome happens before censoring_pair
-  # no censoring must be assigned
-  matched_cohort$censoring_accepted <-
-    as.Date(ifelse(
-      (matched_cohort$censoring_pair > matched_cohort[[outcome_date_col]]) &
-        (!is.na(matched_cohort$censoring_pair)) &
-        (!is.na(matched_cohort[[outcome_date_col]])),
-      as.Date(NA),
-      as.character(matched_cohort$censoring_pair)
-    ))
-
-  # 4. Beginning of the follow-up period for pairs is the immunization date
+  # 1. Beginning of the follow-up period for pairs is the immunization date
   matched_cohort$t0_follow_up <-  as.Date(match_pair_info(
     data_set = matched_cohort,
     column_to_match = immunization_date,
     criteria = "min"
   ))
 
-  # 5. Calculate time-to-event
-  matched_cohort$time_to_event <- get_time_to_event(
+  # 2. Pairs share censoring date when appropiated
+  # (see criteria in get_censoring_pair)
+  matched_cohort$censoring_after_match <- get_censoring_after_match(
     data_set = matched_cohort,
     outcome_date_col = outcome_date_col,
-    censoring_date_col = "censoring_accepted",
-    start_cohort = start_cohort,
-    end_cohort = end_cohort,
-    start_from_immunization = TRUE,
-    immunization_date_col = "t0_follow_up"
+    censoring_date_col = censoring_date_col
   )
 
-  # 6. Individuals with negative exposure and their pairs must be removed
-  matched_cohort$min_exposure_time_pair <- match_pair_info(
+  # 3. Check for exposition times to ensure no negative expositions
+  matched_cohort$exposition_time <- get_exposition_time(
     data_set = matched_cohort,
-    column_to_match = "time_to_event",
+    outcome_date_col = outcome_date_col,
+    censoring_date_col = "censoring_after_match",
+    end_cohort = end_cohort
+  )
+
+  # Individuals with negative exposition and their pairs must be removed
+  matched_cohort$min_exposition_time_pair <- match_pair_info(
+    data_set = matched_cohort,
+    column_to_match = "exposition_time",
     criteria = "min"
   )
 
   adjusted_match <- matched_cohort[
-    matched_cohort$min_exposure_time_pair > 0,
+    matched_cohort$min_exposition_time_pair > 0,
   ]
 
-  # 7. Generate outcome status
-  adjusted_match$outcome_status <- set_event_status(
-    data_set = adjusted_match,
-    outcome_date_col = outcome_date_col,
-    censoring_date_col = "censoring_accepted"
-  )
-
-  # 8. Remove unnecessary columns and reorder
+  # Remove unnecessary columns and reorder
   col_names <- names(adjusted_match)
   col_names <- col_names[! col_names %in%
-      c("censoring_individual",
-        "censoring_pair", "censoring_accepted",
-        "min_exposure_time_pair")
+      c("min_exposition_time_pair", "exposition_time")
   ]
 
   adjusted_match <- subset(adjusted_match, select = col_names)
 
   return(adjusted_match)
+}
+#' @title Calculate Exposition Time After Matching
+#'
+#' @description This auxiliar function calculates the exposition time of
+#' individuals after matching to ensure they coincide. Creating censoring
+#' after match and defining t0_follow_up from pairing information is necessary
+#' before using it.
+#'
+#' @inheritParams match_cohort
+#' @return `column` with exposition time per indivual.
+#' @keywords internal
+
+get_exposition_time <- function(data_set,
+                                outcome_date_col,
+                                censoring_date_col,
+                                end_cohort) {
+  ### Same structure as get_time_to_event
+  tf <- rep(end_cohort, nrow(data_set))
+  # replace informed outcome dates
+  tf <- as.Date(ifelse(!is.na(data_set[[outcome_date_col]]),
+    yes = as.character(data_set[[outcome_date_col]]),
+    no = as.character(tf)
+  ))
+
+  # replace censoring dates if provided
+  if (!is.null(censoring_date_col)) {
+    tf <- as.Date(ifelse(!is.na(data_set[[censoring_date_col]]),
+      yes = as.character(data_set[[censoring_date_col]]),
+      no = as.character(tf)
+    ))
+  }
+
+  #exposition time the difference between tf and t0_follow_up
+  exposition <- as.numeric(tf - data_set$t0_follow_up)
+  return(exposition)
 }

c00226f8065b49dae944cef3bcb8ebe6a483be7a
davidsantiagoquevedo <davidsantiagoquevedo@gmail.com>
2024-11-15 14:59:02 +0100

    grouped into test for handler_validations module

--- tests/testthat/test-handler_validations.R
+++ tests/testthat/test-handler_validations.R
@@ -1,6 +1,30 @@
-#### Tests for check_vaccineff_inputs
-## prepare data
+#### Tests for handler_validations.R module ####
+#### Prepare data for all the tests ####
 data("cohortdata")
+
+#### Tests for check_dataset ####
+# Test for basic expectations
+test_that("`check_dataset`: basic expectations", {
+
+  # passing other type of element to data_set
+  vector <- c("v1", "v2", "v3")
+  expect_error(
+    check_dataset(data_set = vector, columns = vector),
+    regexp =
+      paste0("Assertion on 'data_set' failed: Must be of type 'data.frame',",
+             " not 'character'.")
+  )
+  # No error expected when passing NULL argument in name of cols
+  expect_silent(
+    check_dataset(data_set = cohortdata, columns = c("death_date", NULL, "age"))
+  )
+  # Expect error if columns name not included in data_set
+  expect_error(
+    check_dataset(data_set = cohortdata, columns = "wrong_name")
+  )
+})
+
+#### Tests for check_vaccineff_inputs ####
 data_set <- cohortdata
 outcome_date_col <- "death_date"
 censoring_date_col <- "death_other_causes"

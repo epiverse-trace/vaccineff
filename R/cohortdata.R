83f79309b4287ca6d0f37504d2e0385de3c4dc85
Erika Cantor <63528090+ErikaCantor@users.noreply.github.com>
2024-11-20 12:12:35 -0500

    Update description of cohortdata

--- R/cohortdata.R
+++ R/cohortdata.R
@@ -1,12 +1,24 @@
 #' Cohort data on vaccineff
 #'
-#' Data from
+#' A subset of data from an anonymised, real-world dataset produced as part of
+#' the early stage of the immunization program against COVID-19 in Bogota,
+#' Colombia between February, 2021 and December, 2021. Cohort dataset contains
+#' registers of homologous schemes for two different brands for adults aged 50
+#' years or older. This cohort received two doses of a vaccine aimed at
+#' reducing the risk of death.
+#' The dataset includes disaggregated information on the first and second
+#' vaccine doses (vaccine_date1, vaccine_date2, vaccine1, and vaccine2) for each
+#' participant, along with relevant demographic details (sex and age).
+#' Additionally, the dataset includes the dates of two outcomes: death
+#' associated with COVID-19 (death_date) and death from other causes
+#' (death_other_causes).
+#'
 #'
 #' @docType data
 #'
 #' @usage cohortdata
 #'
-#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#' @format ## `cohortdata`
 #'
 #' @keywords datasets
 #'

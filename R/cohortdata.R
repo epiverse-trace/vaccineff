ef53a0a6c5be852810ee90c9a0f9786c60f0cb67
David Santiago Quevedo <67650252+davidsantiagoquevedo@users.noreply.github.com>
2024-11-22 11:05:06 +0100

    style: description

--- R/cohortdata.R
+++ R/cohortdata.R
@@ -1,18 +1,18 @@
 #' Cohort data on vaccineff
 #'
-#' A subset of data from an anonymised, real-world dataset produced as part of
+#' Subset of data from an anonymised, real-world dataset produced as part of
 #' the early stage of the immunization program against COVID-19 in Bogota,
-#' Colombia between February, 2021 and December, 2021. Cohort dataset contains
+#' Colombia between February 2021 and December 2021. Cohort dataset contains
 #' registers of homologous schemes for two different brands for adults aged 50
 #' years or older. This cohort received two doses of a vaccine aimed at
-#' reducing the risk of death.
+#' reducing the risk of death. All the registers were anonymized and
+#' de-identified to preserve the privacy of data.
 #' The dataset includes disaggregated information on the first and second
 #' vaccine doses (vaccine_date1, vaccine_date2, vaccine1, and vaccine2) for each
-#' participant, along with relevant demographic details (sex and age).
+#' participant and relevant demographic details (sex and age).
 #' Additionally, the dataset includes the dates of two outcomes: death
 #' associated with COVID-19 (death_date) and death from other causes
 #' (death_other_causes).
-#'
 #'
 #' @docType data
 #'

2e5583791d969cc846fa2ba593ece878038c36d3
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-05-12 16:58:32 +0200

    Disable partial matching warning

--- tests/testthat/setup-options.R
+++ tests/testthat/setup-options.R
@@ -1,7 +1,6 @@
 # We want to flag partial matching as part of our testing & continuous
 # integration process because it makes code more brittle.
 options(
-  warnPartialMatchAttr = TRUE,
-  warnPartialMatchDollar = TRUE,
-  warnPartialMatchArgs = TRUE
+  warnPartialMatchAttr = TRUE
+  # The survival package has some partial matching so we don't warn
 )
